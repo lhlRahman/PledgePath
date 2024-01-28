951623b782852188d085229b458e2fc4cb3683a6
lhlRahman <lhlrahman@gmail.com>
2024-01-28 13:59:16 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ client/src/app/page.js
@@ -0,0 +1,10 @@
+'use client';
+import Modal from '../Components/Modal';
+import React from 'react';
+import { useState } from 'react';
+export default function Home() {
+  const [modal, setModal] = useState(true);
+  return (
+   <Modal modal={modal} setModal={setModal} />
+  )
+}
